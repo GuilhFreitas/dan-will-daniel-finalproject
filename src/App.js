b430b08197bc481b116d61784a10a36d21119907
DaniJFerr <daniel_jferreira@hotmail.com>
2023-03-16 23:27:46 +0000

    create HeaderNavbar react component and export props to render handleQuizRequest setNumCards on the App and card section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import React, { useState, useEffect } from "react";
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import Toolbar from '@mui/material/Toolbar';
-import AppBar from '@mui/material/AppBar';
-import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material/Button';
-import MenuIcon from '@mui/icons-material/Menu';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-// import { yellow } from '@mui/material/colors';
-import "./App.css";
 
+import React, { useState, useEffect } from "react";
+import HeaderNavbar from "./component/HeaderNavbar";
+import Box from '@mui/material/Box';
 
 const styles = { 
   hide: { display: "none" },
@@ -22,13 +11,14 @@ const styles = {
 function App() {
   const [quizzes, setQuizzes] = useState([]);
   const [category, setCategory] = useState("");
-  const [limit, setNumCards] = useState();
+  const [numCards, setNumCards] = useState(1);
+  const [mobileOpen, setMobileOpen] = useState(false);
 
   // function to get the quizzes from the API
   const getQuizzes = async () => {
     try {
       const response = await fetch(
-        `https://the-trivia-api.com/api/questions?limit=${limit}&categories=${category}`
+        `https://the-trivia-api.com/api/questions?limit=${numCards}&categories=${category}`
       );
       if (!response.ok) {
         throw new Error("Network response was not ok");
@@ -43,136 +33,47 @@ function App() {
 
   useEffect(() => {
     getQuizzes();
-  }, []);
+  }, [category, numCards]);
 
   const hideQuestionsAndShowAnswers = (event) => {
-    if (event.target.getAttribute("style") === "display: block;") {
-      event.target.setAttribute("style", "display: none;");
-      event.target.nextSibling.setAttribute("style", "display: block;");
-    }
-  };
-
-  const hideAnswersAndShowQuestions = (event) => {
-    if (event.target.getAttribute("style") === "display: block;") {
-      event.target.setAttribute("style", "display: none;");
-      event.target.previousSibling.setAttribute("style", "display: block;");
-    }
-  };
-
-  const handleCategoryChange = (event) => {
-    setCategory(event.target.value);
-  };
-
-  const handleNumCardsChange = (event) => {
-    setNumCards(Number.parseInt(event.target.value));
-  };
+        if (event.target.getAttribute("style") === "display: block;") {
+          event.target.setAttribute("style", "display: none;");
+          event.target.nextSibling.setAttribute("style", "display: block;");
+        }
+      };
+    
+      const hideAnswersAndShowQuestions = (event) => {
+        if (event.target.getAttribute("style") === "display: block;") {
+          event.target.setAttribute("style", "display: none;");
+          event.target.previousSibling.setAttribute("style", "display: block;");
+        }
+      };
+    
 
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
   };
-  
 
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const handleQuizRequest = (newCategory, newNumCards) => {
+    setCategory(newCategory);
+    setNumCards(newNumCards);
+  };
 
-  const nav = createTheme({
-    palette: {
-      primary: {
-      main: '#fff59d',   
-      },
-    },
-  });
-
-  const btn = createTheme({
-    palette: {
-      primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
-      contrastText: '#fff',
-      },
-    },
-  });
-
-  
-  const dropDown = createTheme({
-    palette: {
-      primary: {
-      main: '#03a9f4',
-      },
-    },
-  });
-  
-  
-  
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Quiz Time</h1>
-      </header>
+      <HeaderNavbar onQuizRequest={handleQuizRequest} onDrawerToggle={handleDrawerToggle} onNumCardsChange={setNumCards} />
       <Box onClick={handleDrawerToggle} sx={{ flexGrow: 1 }}>
-      <ThemeProvider theme={nav}>
-      <AppBar position='static' >
-      <Toolbar> 
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-             <MenuIcon />
-          </IconButton>
-
-      <ThemeProvider theme={btn}>
-        <Button onClick={getQuizzes}variant="contained" >Generate Quiz</Button>
-      </ThemeProvider>
-
-      <ThemeProvider theme={dropDown}>
-        <Box sx={{ minWidth: 120, margin: 2}}>
-         <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label" label="Category">Category</InputLabel>
-          <Select labelId="demo-simple-select-label" id="demo-simple-select" value={category} label="Category" onChange={handleCategoryChange}>
-          <MenuItem value="Arts & Literature" >Arts & Literature</MenuItem>
-            <MenuItem value="Film & TV">Film & TV</MenuItem>
-            <MenuItem value="science">Science</MenuItem>
-            <MenuItem value="history">History</MenuItem>
-            <MenuItem value="Society & Culture">Society & Culture</MenuItem>
-            <MenuItem value="Geography">Geography</MenuItem>
-          </Select>
-         </FormControl>
-        </Box>
-        </ThemeProvider>
-        
-    <ThemeProvider theme={dropDown}>
-      <Box sx={{ minWidth: 125 }}>
-        <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Cards:</InputLabel>
-          <Select labelId="demo-simple-select-label" id="demo-simple-select" value={limit} label="Cards" onChange={handleNumCardsChange}>
-            <MenuItem value="1">1</MenuItem>
-            <MenuItem value="5">5</MenuItem>
-            <MenuItem value="10">10</MenuItem>
-            <MenuItem value="15">15</MenuItem>
-          </Select>
-          </FormControl>
-          </Box>
-          </ThemeProvider> 
-          </Toolbar>
-          </AppBar>
-          </ThemeProvider>      
-        </Box>
-      
-     
- 
-      {quizzes.map((quiz) => (
-        <div key={quiz.id}>
-          <p style={styles.show} onClick={(event) => hideQuestionsAndShowAnswers(event)}>
-            Question: {quiz.question}
-          </p>
-          <p style={styles.hide} onClick={(event) => hideAnswersAndShowQuestions(event)}>
-            Answer: {quiz.correctAnswer}
-          </p>
-        </div>
-      ))}
+        {quizzes.map((quiz) => (
+          <div key={quiz.id}>
+            <p style={styles.show} onClick={(event) => hideQuestionsAndShowAnswers(event)}>
+              Question: {quiz.question}
+            </p>
+            <p style={styles.hide} onClick={(event) => hideAnswersAndShowQuestions(event)}>
+              Answer: {quiz.correctAnswer}
+            </p>
+          </div>
+        ))}
+      </Box>
     </div>
   );
 }
---
+++ src/component/HeaderNavbar.jsx
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Toolbar from '@mui/material/Toolbar';
+import AppBar from '@mui/material/AppBar';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import MenuIcon from '@mui/icons-material/Menu';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import "./HeaderNavbar.css";
+
+const styles = { 
+  hide: { display: "none" },
+  show: { display: "block" }
+};
+
+function HeaderNavbar(props) {
+  const [category, setCategory] = useState("");
+  const [limit, setNumCards] = useState();
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const handleNumCardsChange = (event) => {
+    setNumCards(Number.parseInt(event.target.value));
+    props.onNumCardsChange(Number.parseInt(event.target.value));
+  };
+
+  const handleDrawerToggle = () => {
+    props.onDrawerToggle();
+  };
+  
+
+  const nav = createTheme({
+    palette: {
+      primary: {
+        main: '#fff59d',   
+      },
+    },
+  });
+
+  const btn = createTheme({
+    palette: {
+      primary: {
+        light: '#757ce8',
+        main: '#2196f3',
+        dark: '#002884',
+        contrastText: '#fff',
+      },
+    },
+  });
+
+  
+  const dropDown = createTheme({
+    palette: {
+      primary: {
+        main: '#03a9f4',
+      },
+    },
+  });
+  
+  return (
+    <div className="HeaderNav">
+
+    <header className="App-header">
+      <div className="App-title">
+        <h2 className="neonText">Quiz</h2>
+        <h1 className="neonText">Time</h1>
+        </div>
+      </header>
+
+
+    <Box sx={{ flexGrow: 1 }}>
+      <ThemeProvider theme={nav}>
+        <AppBar position='static'>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: 'none' } }}
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <ThemeProvider theme={btn}>
+              <Button onClick={() => props.onQuizRequest(category, limit)} variant="contained">Quiz</Button>
+            </ThemeProvider>
+
+            <ThemeProvider theme={dropDown}>
+              <Box sx={{ minWidth: 120, margin: 2}}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label" label="Category">Category</InputLabel>
+                  <Select labelId="demo-simple-select-label" id="demo-simple-select" value={category} label="Category" onChange={handleCategoryChange}>
+                    <MenuItem value="Arts & Literature">Arts & Literature</MenuItem>
+                    <MenuItem value="Film & TV">Film & TV</MenuItem>
+                    <MenuItem value="science">Science</MenuItem>
+                    <MenuItem value="history">History</MenuItem>
+                    <MenuItem value="Society & Culture">Society & Culture</MenuItem>
+                    <MenuItem value="Geography">Geography</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+            </ThemeProvider>
+        
+            <ThemeProvider theme={dropDown}>
+              <Box sx={{ minWidth: 125 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Cards:</InputLabel>
+                  <Select labelId="demo-simple-select-label" id="demo-simple-select" value={limit} label="Cards" onChange={handleNumCardsChange}>
+                    <MenuItem value="1">1</MenuItem>
+                    <MenuItem value="5">5</MenuItem>
+                    <MenuItem value="10">10</MenuItem>
+                    <MenuItem value="15">15</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+            </ThemeProvider> 
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
+    </Box>
+    </div>
+  );
+}
+
+export default HeaderNavbar;
