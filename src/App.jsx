d44cdefd4da9f6f7e79fa001d072a8410b737c29
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-03-17 15:46:31 +0000

    Complete the merge with daniel's first merge and remove the dependencies issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Main from "./components/Main";
+import HeaderNavbar from "./components/HeaderNavbar";
 
 function App() {
   return (
     <div className="App">
+      <HeaderNavbar />
       <Main />
     </div>
   );
