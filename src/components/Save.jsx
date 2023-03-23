177289f72b44e1d3236eca3013161a46a73f6ebe
GuillhFreitas <guilherme.antraco@gmail.com>
2023-03-23 09:55:04 +0000

    Revert "add Dialog to display options for saving"
    This reverts commit 45e10d1260b0480c9db2b72414b8dc9fe8ddfd92.

--- src/components/Save.jsx
+++ src/components/Save.jsx
@@ -1,141 +1,4 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import Dialog from '@mui/material/Dialog';
-import DialogContent from '@mui/material/DialogContent';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-
-// function sents POST request to AnkiConnect
-async function invoke(action, version, params = {}) {
-    const response = await fetch('http://127.0.0.1:8765', {
-        method: "POST",
-        mode: "cors",
-        cache: "no-cache",
-        credentials: "same-origin",
-        body: JSON.stringify({ action, version, params }),
-
-    });
-    return response.json();
-};
-
-function SaveMenu(props) {
-
-}
-
-// defines the dialog box
-function SaveDialog(props) {
-
-    // const { onClose, selectedValue, open } = props;
-
-    // const handleClose = () => {
-    //     onClose(selectedValue);
-    // };
-
-    // const handleListItemClick = (value) => {
-    //     onClose(value);
-    // };
-
-    // return (
-    //     <Dialog
-    //         fullWidth
-    //         maxWidth="xs"
-    //         onClose={handleClose}
-    //         open={open}
-    //     >
-    //         <DialogContent
-    //             style={{height:'200px'}}
-    //         >
-    //             <DialogTitle>Set backup account</DialogTitle>
-    //             <List sx={{ pt: 0 }}>
-    //                 {emails.map((email) => (
-    //                     <ListItem disableGutters>
-    //                         <ListItemButton onClick={() => handleListItemClick(email)} key={email}>
-    //                             <ListItemAvatar>
-    //                                 <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
-    //                                     <PersonIcon />
-    //                                 </Avatar>
-    //                             </ListItemAvatar>
-    //                             <ListItemText primary={email} />
-    //                         </ListItemButton>
-    //                     </ListItem>
-    //                 ))}
-
-    //                 <ListItem disableGutters>
-    //                     <ListItemButton
-    //                         autoFocus
-    //                         onClick={() => handleListItemClick('addAccount')}
-    //                     >
-    //                         <ListItemAvatar>
-    //                             <Avatar>
-    //                                 <AddIcon />
-    //                             </Avatar>
-    //                         </ListItemAvatar>
-    //                         <ListItemText primary="Add account" />
-    //                     </ListItemButton>
-    //                 </ListItem>
-    //             </List>
-    //         </DialogContent>
-
-    //     </Dialog>
-    // );
-
-    const { onClose, open } = props;
-
-    const handleClose = () => {
-        onClose();
-    };
-
-    const handleListItemClick = (value) => {
-        // placeholder for txt file save
-        if (value === "txt"){
-            return
-        }else if (value === "anki"){
-            invoke()
-        }
-    };
-
-    return (
-        <Dialog
-            fullWidth
-            maxWidth="xs"
-            onClose={handleClose}
-            open={open}>
-            <DialogContent
-            >
-                <List sx={{ pt: 0 }}>
-                    <ListItem disableGutters>
-                        <ListItemButton onClick={() => handleListItemClick("txt")}>
-                            <ListItemText primary="Save as txt file" />
-                        </ListItemButton>
-                    </ListItem>
-
-                    <ListItem disableGutters>
-                        <ListItemButton onClick={() => handleListItemClick("anki")}>
-                            <ListItemText primary="Save with AnkiConnect" />
-                        </ListItemButton>
-                    </ListItem>
-                </List>
-            </DialogContent>
-
-        </Dialog>
-    );
-}
-
-// sets data type for function above
-SaveDialog.propTypes = {
-    onClose: PropTypes.func.isRequired,
-    open: PropTypes.bool.isRequired,
-
-};
-
-export default function Save(props) {
-
-    // button style
+export default function Save(props){
     const styles = {
         backgroundColor: "red",
         height: "50px",
@@ -145,38 +8,29 @@ export default function Save(props) {
         textAlign: "center"
     }
 
-    // functions for dialog box
-    const [open, setOpen] = React.useState(false);
-    // const [selectedValue, setSelectedValue] = React.useState(emails[1]);
+    async function saveCard(){
+        async function invoke(action, version, params={}) {
+            const response = await fetch ('http://127.0.0.1:8765', {
+                method: "POST",
+                mode: "cors",
+                cache: "no-cache",
+                credentials: "same-origin",
+                body: JSON.stringify({action, version, params}),
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = (value) => {
-        setOpen(false);
-        // setSelectedValue(value);
-    };
-
-
-    async function saveCard() {
-
-        await invoke('requestPermission', 6);
-        await invoke('addNote', 6, { deck: 'test1' });
+            });
+            return response.json();
+            };
+        
+        await invoke ('requestPermission', 6);
+        await invoke('createDeck', 6, {deck: 'test1'});
         const result = await invoke('deckNames', 6);
         console.log(`got list of decks: ${result}`);
     }
 
 
-    return (
-        <div>
-            <Button style={styles} onClick={handleClickOpen}>
-                💾
-            </Button>
-            <SaveDialog
-                open={open}
-                onClose={handleClose}
-            />
-        </div>
+    return(
+        <button style={styles} onClick={saveCard}>
+            💾
+        </button>
     )
 }
